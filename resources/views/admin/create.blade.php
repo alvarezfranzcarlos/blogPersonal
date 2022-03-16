a8ebeae635924115aa5cbaa60a14e0c303f31c5c
alvarezfranzcarlos <74078909+alvarezfranzcarlos@users.noreply.github.com>
2022-03-16 04:21:48 -0400

    Video 8 final

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -19,10 +19,5 @@
             </form>
         </div>
     </div>
-    <hr>
-    <div class="row">
-        <div class="col-md-12">
-            <p><strong>Aprendiendo Laravel</strong> <a ref="">Edit</a></p>
-        </div>
-    </div>
+
 @endsection
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.master')
 @section('content')
 
+<div class="card" >
+    <div class="card-body">
+      <h5 class="card-title">Nombre articulo</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Ver mas</a>
+    </div>
+  </div>
+
+  <div class="card" >
+    <div class="card-body">
+      <h5 class="card-title">Nombre articulo</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Ver mas</a>
+    </div>
+  </div>
+
+  <div class="card" >
+    <div class="card-body">
+      <h5 class="card-title">Nombre articulo</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Ver mas</a>
+    </div>
+  </div>
 @endsection
