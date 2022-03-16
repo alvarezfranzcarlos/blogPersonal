4aeba56d8190b685270770f99c60a94c8d3c58f6
alvarezfranzcarlos <74078909+alvarezfranzcarlos@users.noreply.github.com>
2022-03-16 03:52:51 -0400

    Video 7 final

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+@include('partials.errors')
+
+    <div class="row">
+        <div class="col-md-12">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <input type="text" class="form-control" id="content" name="content">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <p><strong>Aprendiendo Laravel</strong> <a ref="">Edit</a></p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <a ref="" class="btn btn-success">New Post</a>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-md-12">
+        <p><strong>Aprendiendo Laravel</strong> <a ref="">Edit</a></p>
+    </div>
+</div>
+@endsection
