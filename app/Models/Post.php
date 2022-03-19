ba4f266992a9aa8036db3a31be6494e50300ea58
alvarezfranzcarlos <74078909+alvarezfranzcarlos@users.noreply.github.com>
2022-03-19 06:57:35 -0400

    Video 26 Final ultimo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model{
 
+
+    protected $fillable = ['title', 'content'];
+
     public function getPosts($session){
         if(!$session->has('posts')){
             $this->createDummyData($session);
